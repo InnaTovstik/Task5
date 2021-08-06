d451124fd6a8b940865f4930fbf071ad069b44d7
InnaTovstik <kitimail69@gmail.com>
2021-08-06 21:26:52 +0300

    Add print "Commit 7. Branch feature-2-bugfix"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Commit 2. Branch master");
         System.out.println("Commit 5. Branch feature-2");
+        System.out.println("Commit 7. Branch feature-2-bugfix");
     }
 }
